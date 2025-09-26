c2b4ec921200739027a7ec2ea1861d888047d370
Shema <shemaphilbert8@gmail.com>
2025-09-26 07:45:38 +0200

    first commit

---
+++ keyword-and-identifier.c
@@ -0,0 +1,7 @@
+#include <stdio.h>   // keyword: #include
+int main() {         // keywords: int, main
+    int number = 10; // keyword: int
+    printf("%d", number);
+    return 0;        // keyword: return
+}
+
